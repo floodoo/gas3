74dcd471991b573e67d0d02c69a9f6f6c7c252cd
Florian Schmitz <florian@ruediger-schmitz.de>
2020-11-16 20:09:39 +0100

    Sound stoppt wenn Handy aus ist

--- lib/homePage/homepage.dart
+++ lib/homePage/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gas3/homePage/kmhTextDisplay.dart';
@@ -11,14 +12,15 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   double speedInMps;
   double speedInKmh;
   String speedInKmhString;
   var geolocator = Geolocator();
   var locationOptions =
       LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
-  static AudioCache player = AudioCache();
+  static AudioCache cache = AudioCache();
+  AudioPlayer player;
 
   Future<void> getVehicleSpeed() async {
     Geolocator.getPositionStream().listen((position) async {
@@ -33,15 +35,45 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     getVehicleSpeed();
     Screen.keepOn(true);
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
-    player.loop("audio/gas.mp3");
+    play();
+  }
+
+  void play() async {
+    player = await cache.loop("audio/gas.mp3");
   }
 
   @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    switch (state) {
+      case AppLifecycleState.resumed:
+        player.resume();
+        break;
+      case AppLifecycleState.inactive:
+        player.pause();
+        break;
+      case AppLifecycleState.paused:
+        player.pause();
+        break;
+      case AppLifecycleState.detached:
+        break;
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
