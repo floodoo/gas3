c0d79040bb5bc570a9a75caff540e6ebb596c867
Florian Schmitz <florian@ruediger-schmitz.de>
2021-04-14 22:39:52 +0200

    music file picker and some changes

--- build/app/generated/source/buildConfig/release/com/example/gas3/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/example/gas3/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.example.gas3";
   public static final String BUILD_TYPE = "release";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
 }
